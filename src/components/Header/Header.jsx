ebb9194d24f7f0ff7df2f490b99b7eb085b6990d
borismg04 <borismg04@gmail.com>
2022-07-18 09:59:06 -0500

    images

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,14 +1,22 @@
 import React from 'react'
-// import images from '../../assets/images'
+import images from '../../assets/images'
 import styles from './Header.module.css'
 
 const Header = () => {
   return (
       <div className={styles.container}>
         <div className={styles.contailner_title}>
-          <h2>Descubra nuevos </h2>
-          <h2>destinos.</h2>
+          <p>Descubra nuevos destinos.</p>
         </div>
+
+        <div className={styles.icon_logo}>
+          <img src={images.logo} alt="logo"/>
+        </div>
+
+        <div className={styles.container_icons}>
+            <img src={images.search} alt="search"/>
+            <img src={images.user} alt="user"/>
+        </div> 
       </div>
   )
 }
